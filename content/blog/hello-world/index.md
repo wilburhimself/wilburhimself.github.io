3090e45985ec808b664657b5d8767dde0ba364dd
Wilbur Suero <wilbur.himself@gmail.com>
2019-01-01 23:33:37 -0400

    change description and profile pic

--- content/blog/hello-world/index.md
+++ content/blog/hello-world/index.md
@@ -10,6 +10,10 @@ I'm sure I'll write a lot more interesting things in the future.
 Oh, and here's a great quote from this Wikipedia on
 [salted duck eggs](http://en.wikipedia.org/wiki/Salted_duck_egg).
 
+```ruby
+rspec spec/controllers/home_controller_spec.rb
+```
+
 > A salted duck egg is a Chinese preserved food product made by soaking duck
 > eggs in brine, or packing each egg in damp, salted charcoal. In Asian
 > supermarkets, these eggs are sometimes sold covered in a thick layer of salted
--- src/components/Bio.js
+++ src/components/Bio.js
@@ -28,8 +28,7 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
+              Written by <strong>{author}</strong> who lives and works in Dominican Republic building useful things.
               {` `}
               <a href={`https://twitter.com/${social.twitter}`}>
                 You should follow him on Twitter
@@ -44,7 +43,7 @@ function Bio() {
 
 const bioQuery = graphql`
   query BioQuery {
-    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+    avatar: file(absolutePath: { regex: "/profile-pic.jpeg/" }) {
       childImageSharp {
         fixed(width: 50, height: 50) {
           ...GatsbyImageSharpFixed
