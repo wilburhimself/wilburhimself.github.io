191ade92b7b28ac0ecf6484a1d9fc86655dc2706
Wilbur Suero <wilbur.himself@gmail.com>
2019-01-01 23:59:20 -0400

    update ga analutics trcking #

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-131537657-1`,
       },
     },
     `gatsby-plugin-feed`,
